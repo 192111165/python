6117f69196f987938cb703bbf9567ef0abaaec33
192111165 <112369538+192111165@users.noreply.github.com>
2022-08-30 13:21:41 +0530

    Add files via upload

---
+++ reverse word.py
@@ -0,0 +1,10 @@
+def reverse_string(str):
+    str1=("")
+    for i in str:
+        str1=i+str1
+    return str1
+str="guna"
+print("the original atring is: ",str)
+print("thr reverse string is: ",reverse_string(str))
+
+    
